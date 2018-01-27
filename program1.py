86ffa3f12222a290ccf6e8ef34bfd69766609050
deepanshut041 <deepanshut041@gmail.com>
2018-01-27 14:38:51 +0530

    started working on triplet program

---
+++ program1.py
@@ -0,0 +1,8 @@
+a = [5, 6, 8, 14, 86, 45, 43, 24, 67, 34, 56, 50]
+new_array = []
+
+for i in range(0,len(a)):
+    for j in range(i + 1 , len(a)-1):
+        new_array.append([a[i], a[j]])
+
+print(new_array)
