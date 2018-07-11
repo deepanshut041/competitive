cba00ffdc3fbad6736235e93cfcf62e9ee2f53f0
deepanshut041 <deepanshut041@gmail.com>
2018-07-11 22:00:51 +0530

    Working on delete at a postion in singly linked llist

--- hacker_rank/data structures/linked_list/singly_linked_list.cpp
+++ hacker_rank/data structures/linked_list/singly_linked_list.cpp
@@ -47,6 +47,71 @@ class SinglyLinkedList
     }
 
   public:
+    void insertAtTop(int value)
+    {
+        SinglyLinkedListNode *newNode = new SinglyLinkedListNode(value);
+        if (!this->head)
+        {
+            this->head = newNode;
+        }
+        else
+        {
+            SinglyLinkedListNode *node = this->head;
+            this->head = newNode;
+            this->head->next = node;
+        }
+    }
+
+  public:
+    void insertAtPosition(int value, int pos)
+    {
+        int count = 1;
+        SinglyLinkedListNode *newNode = new SinglyLinkedListNode(value);
+        if (!this->head)
+        {
+            if (pos == 0)
+            {
+                head = newNode;
+            }
+        }
+        else
+        {
+            SinglyLinkedListNode *node = this->head;
+            while (node->next != nullptr)
+            {
+                if (count == pos)
+                {
+                    newNode->next = node->next;
+                    node->next = newNode;
+                    break;
+                }
+                count++;
+                node = node->next;
+            }
+        }
+    }
+
+  public:
+    void deletAtPosition(int pos)
+    {
+        int count = 0;
+        if (!this->head)
+        {
+            SinglyLinkedListNode *node = this->head;
+            while (node->next != nullptr)
+            {
+                if (count == pos - 1)
+                {
+                    node->next = node->next->next;
+                    break;
+                }
+                count++;
+                node = node->next;
+            }
+        }
+    }
+
+  public:
     void printList()
     {
         if (this->head)
@@ -73,8 +138,10 @@ int main()
     cout << "SinglyLinkedList program \n";
     while (a != 0)
     {
-        cout << "1: to insert a number \n";
-        cout << "2: to print list \n";
+        cout << "1: to insert a number at bottom \n";
+        cout << "2: to insert a number at top \n";
+        cout << "3: to insert a number at position as: <position> <value> \n";
+        cout << "4: to print list \n";
         cin >> a;
         switch (a)
         {
@@ -87,6 +154,17 @@ int main()
             break;
 
         case 2:
+            int t;
+            cin >> t;
+            list->insertAtTop(t);
+            break;
+        case 3:
+            int n, p;
+            cin >> p >> n;
+            list->insertAtPosition(n, p);
+            break;
+
+        case 4:
             list->printList();
             break;
 
