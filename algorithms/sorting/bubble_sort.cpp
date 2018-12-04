1aae4911895c4cc9844409393e690fb1288da316
deepanshut041 <deepanshut041@gmail.com>
2018-12-04 20:27:20 +0530

    bubble sort  implemented now working on insertion sort

--- algorithms/sorting/bubble_sort.cpp
+++ algorithms/sorting/bubble_sort.cpp
@@ -1,5 +1,4 @@
-//  Selection sort array is sorted from one end until complete array is sorted. First maximun number
-//  is set to last variable and process continues
+//  First maximun number is set to last variable and process continues
 
 #include <iostream>
 #include <math.h>
@@ -27,13 +26,13 @@ int main()
     print_array(array);
     for (int i = 0; i < length; i++)
     {
-        for (int j = i + 1; j < length; j++)
+        for (int j = 0; j < length - 1 - i; j++)
         {
-            if (array[i] > array[j])
+            if (array[j] > array[j + 1])
             {
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
             }
         }
         cout << "Iteration " << i + 1 << ": ";
