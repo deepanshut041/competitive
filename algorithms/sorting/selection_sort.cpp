28a0bf22bdbac34b1493832b2018f7f9a11fb2d2
deepanshut041 <deepanshut041@gmail.com>
2018-12-04 20:18:18 +0530

    Completed selction sort now working on insertion sort

---
+++ algorithms/sorting/selection_sort.cpp
@@ -0,0 +1,46 @@
+//  Selection sort array is sorted from one end until complete array is sorted. First maximun number
+//  is set to last variable and process continues
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+const int length = 10;
+
+void print_array(int (&arr)[length]){
+    for (int a : arr)
+    {
+        cout << a << ", ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int array[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rand() % 100;
+    }
+    cout << "Unsorted Array: ";
+    print_array(array);
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+        cout << "Iteration " << i + 1 << ": ";
+        print_array(array);
+    }
+    cout << "Sorted Array: ";
+    print_array(array);
+
+    return 0;
+}
