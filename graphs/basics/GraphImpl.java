f3b4f3c0159cd558ed3264616d4cb4a9ad5d1328
deepanshut041 <deepanshut041@gmail.com>
2018-12-26 03:34:54 +0530

    Completed topological sort

--- graphs/basics/GraphImpl.java
+++ graphs/basics/GraphImpl.java
@@ -1,53 +1,17 @@
 // Java Graph implementaion using adjacency list
-import java.util.*;
-
 public class GraphImpl{
-    static class Graph{
-
-        // Array of vertices in graph
-        LinkedList<Integer> adjacencyList[];
-        // length of array
-        final int V;
-
-        Graph(int V){
-            this.V = V;
-            adjacencyList = new LinkedList[this.V];
-            for (int i = 0; i < V; i++) {
-                // Intilizing ever item array with empty linkedlist
-                adjacencyList[i] = new LinkedList<>();
-            }
-        }
-    }
-
-    static void addEdge(Graph graph, int v, int u){
-        graph.adjacencyList[v].add(u);
-        graph.adjacencyList[u].add(v);
-    }
-
-    static void printGraph(Graph graph){
-        for(int v = 0; v < graph.V; v++) 
-        { 
-            System.out.println("Adjacency list of vertex "+ v); 
-            System.out.print("head"); 
-            for(Integer node: graph.adjacencyList[v]){ 
-                System.out.print(" -> "+node); 
-            } 
-            System.out.println("\n"); 
-        } 
-    }
-
 
     public static void main(String args[]){
         // create the graph given in above figure 
         int V = 5; 
         Graph graph = new Graph(V); 
-        addEdge(graph, 0, 1); 
-        addEdge(graph, 0, 4); 
-        addEdge(graph, 1, 2); 
-        addEdge(graph, 1, 3); 
-        addEdge(graph, 1, 4); 
-        addEdge(graph, 2, 3); 
-        addEdge(graph, 3, 4); 
-        printGraph(graph);
+        graph.addEdge(0, 1); 
+        graph.addEdge(0, 4); 
+        graph.addEdge(1, 2); 
+        graph.addEdge(1, 3); 
+        graph.addEdge(1, 4); 
+        graph.addEdge(2, 3); 
+        graph.addEdge(3, 4); 
+        System.out.println(graph.toString());
     }
  }
